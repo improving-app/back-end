d406326c57403d924536d46d99e046052c4eefe5
Matthias Howell <matthias.howell@yoppworks.com>
2022-04-08 17:32:46 -0400

    removed duplicate entry

--- scripts/ddl/query.sql
+++ scripts/ddl/query.sql
@@ -3,5 +3,3 @@ DROP TABLE IF EXISTS low_inventory CASCADE;
 DROP TABLE IF EXISTS akka_projection_management CASCADE;
 
 DROP TABLE IF EXISTS akka_projection_offset_store CASCADE;
-
-DROP TABLE IF EXISTS low_inventory CASCADE;
