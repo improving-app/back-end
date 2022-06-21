bd525230dc48339a9f7fc05f6eb0de35473ffad1
Matthias Howell <matthias.howell@yoppworks.com>
2022-06-21 11:01:54 -0400

    another successful compile with first message

--- member/src/main/scala/com/improving/app/member/domain/Member.scala
+++ member/src/main/scala/com/improving/app/member/domain/Member.scala
@@ -4,19 +4,34 @@ import com.improving.app.member.api
 import kalix.scalasdk.eventsourcedentity.EventSourcedEntity
 import kalix.scalasdk.eventsourcedentity.EventSourcedEntityContext
 
+import java.time.Instant
+
 // This class was initially generated based on the .proto definition by Kalix tooling.
 //
 // As long as this file exists it will not be overwritten: you can maintain it yourself,
 // or delete it so it is regenerated as needed.
 
 class Member(context: EventSourcedEntityContext) extends AbstractMember {
-  override def emptyState: MemberState =
-    throw new UnsupportedOperationException("Not implemented yet, replace with your empty entity state")
+  override def emptyState: MemberState = MemberState.defaultInstance
+
 
   override def registerMember(currentState: MemberState, registerMember: api.RegisterMember): EventSourcedEntity.Effect[api.MemberRegistered] =
-    effects.error("The command handler for `registerMember` is not implemented, yet")
+    if (currentState == emptyState) {
+      val meta = api.MemberMetaInfo(createdOn = Instant.now().toEpochMilli, createdBy = registerMember.registeringMember, memberState = api.MemberState.Active)
+      val event = for {
+        memToAdd <- registerMember.memberToAdd
+      }yield api.MemberRegistered(memToAdd.memberId, memToAdd.memberInfo, Some(meta))
+      if (event.isDefined)
+        effects.emitEvent(event.get).thenReply(_ => event.get)
+      else
+        effects.error(s"Member ${registerMember.memberToAdd.map(x => x.memberId).getOrElse("missing member Id")} is missing data")
+    } else {
+      effects.error(s"Member ${registerMember.memberToAdd.map(x => x.memberId).getOrElse("missing member Id")} has already been registered")
+    }
+
+    //effects.error("The command handler for `registerMember` is not implemented, yet")
 
   override def memberRegistered(currentState: MemberState, memberRegistered: api.MemberRegistered): MemberState =
-    throw new RuntimeException("The event handler for `MemberRegistered` is not implemented, yet")
+    currentState.copy(memberId = memberRegistered.memberId, memberInfo = memberRegistered.memberInfo, memberMetaInfo = memberRegistered.memberMetaInfo)
 
 }
