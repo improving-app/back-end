4cf0029a98f2772065b7782ac4afa43e6689e9ad
miguel-taningco <miguel.taningco@yoppworks.com>
2023-03-13 13:17:11 -0700

    Remove print statements.

--- tenant/src/main/scala/com/improving/app/tenant/TenantServiceImpl.scala
+++ tenant/src/main/scala/com/improving/app/tenant/TenantServiceImpl.scala
@@ -10,11 +10,9 @@ import scala.concurrent.duration.DurationInt
 
 class TenantServiceImpl(sys: ActorSystem[TestActor.TestCommand]) extends TenantService {
   private implicit val system: ActorSystem[TestActor.TestCommand] = sys
-  println("created TenantServiceImpl")
 
   val testActor: ActorRef[TestActor.TestCommand] = system
   override def testFunction(in: TestInput): Future[TestOutput] = {
-    println("inside testFunction")
     implicit val timeout: Timeout = 3.seconds
     val result: Future[TestEvent] = testActor.ask(ref => TestCommand(in.myInput, ref))
     implicit val ec = system.executionContext
