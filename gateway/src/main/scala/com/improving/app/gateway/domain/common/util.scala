8bd479c97dbe345d29aa9e1e86084b31fa20edcc
alexweinstein <alex.weinstein@improving.com>
2023-04-12 14:13:32 -0400

    fix: move test to gateway, various fixes to get it close to working [IA-161]

--- gateway/src/main/scala/com/improving/app/gateway/domain/common/util.scala
+++ gateway/src/main/scala/com/improving/app/gateway/domain/common/util.scala
@@ -1,5 +1,23 @@
 package com.improving.app.gateway.domain.common
 
+import com.improving.app.common.domain.{Contact, MemberId, OrganizationId, TenantId}
+import com.improving.app.gateway.domain.MemberMessages.{
+  ErrorResponse,
+  MemberData,
+  MemberEventResponse,
+  MemberRegistered
+}
+import com.improving.app.gateway.domain.MemberStatus.{
+  ACTIVE_MEMBER_STATUS,
+  DRAFT_MEMBER_STATUS,
+  SUSPENDED_MEMBER_STATUS
+}
+import com.improving.app.gateway.domain.NotificationPreference.{
+  APPLICATION_NOTIFICATION_PREFERENCE,
+  EMAIL_NOTIFICATION_PREFERENCE,
+  SMS_NOTIFICATION_PREFERENCE
+}
+import com.improving.app.gateway.domain.common.{Contact => GatewayContact}
 import com.improving.app.member.domain.{MemberInfo, MemberMetaInfo, NotificationPreference}
 import com.typesafe.config.ConfigFactory
 import com.improving.app.gateway.domain.{
@@ -48,6 +66,12 @@ object util {
     info.tenant
   )
 
+  private def memberStateToGatewayMemberState(memberStatus: MemberState): GatewayMemberStatus = {
+    if (memberStatus.isMemberStatusDraft) DRAFT_MEMBER_STATUS
+    else if (memberStatus.isMemberStatusActive) ACTIVE_MEMBER_STATUS
+    else SUSPENDED_MEMBER_STATUS
+  }
+
   def memberMetaToGatewayMemberMeta(meta: MemberMetaInfo): GatewayMemberMetaInfo = GatewayMemberMetaInfo(
     meta.createdOn,
     meta.createdBy,
@@ -55,4 +79,46 @@ object util {
     meta.lastModifiedBy,
     MemberStates.fromValue(meta.currentState.value)
   )
+
+  private def notificationPreferenceToGatewayNotificationPreference(
+      notificationPreferenceOpt: Option[NotificationPreference]
+  ): GatewayNotificationPreference =
+    notificationPreferenceOpt.fold[GatewayNotificationPreference](APPLICATION_NOTIFICATION_PREFERENCE)(
+      notificationPreference =>
+        if (notificationPreference.isNotificationPreferenceEmail) EMAIL_NOTIFICATION_PREFERENCE
+        else if (notificationPreference.isNotificationPreferenceSms) SMS_NOTIFICATION_PREFERENCE
+        else APPLICATION_NOTIFICATION_PREFERENCE
+    )
+
+  private def gatewayNotificationPreferenceToNotificationPreference(
+      notificationPreference: GatewayNotificationPreference
+  ): NotificationPreference = notificationPreference match {
+    case EMAIL_NOTIFICATION_PREFERENCE =>
+      NotificationPreference.NOTIFICATION_PREFERENCE_EMAIL
+    case SMS_NOTIFICATION_PREFERENCE =>
+      NotificationPreference.NOTIFICATION_PREFERENCE_SMS
+    case APPLICATION_NOTIFICATION_PREFERENCE =>
+      NotificationPreference.NOTIFICATION_PREFERENCE_APPLICATION
+  }
+
+  def memberResponseToGatewayEventResponse(
+      response: com.improving.app.member.domain.MemberResponse
+  ): MemberEventResponse =
+    if (response.asMessage.sealedValue.isMemberEventValue) {
+      if (response.asMessage.getMemberEventValue.memberEvent.asMessage.sealedValue.isMemberRegisteredValue) {
+        val registered = response.asMessage.getMemberEventValue.memberEvent.asMessage.getMemberRegisteredValue
+        MemberRegistered(
+          UUID.fromString(registered.memberId.getOrElse(MemberId.defaultInstance).id),
+          memberInfoToGatewayMemberInfo(registered.memberInfo.getOrElse(MemberInfo.defaultInstance)),
+          memberMetaToGatewayMemberMeta(registered.meta.getOrElse(MemberMetaInfo.defaultInstance))
+        )
+      } else ErrorResponse("MemberEventResponse type is not yet implemented")
+    } else {
+      val data = response.asMessage.getMemberStateValue
+      MemberData(
+        UUID.fromString(data.memberId.getOrElse(MemberId.defaultInstance).id),
+        memberInfoToGatewayMemberInfo(data.memberInfo.getOrElse(MemberInfo.defaultInstance)),
+        memberMetaToGatewayMemberMeta(data.memberMetaInfo.getOrElse(MemberMetaInfo.defaultInstance))
+      )
+    }
 }
--- gateway/src/test/scala/com/improving/app/gateway/GatewayServerSpec.scala
+++ gateway/src/test/scala/com/improving/app/gateway/GatewayServerSpec.scala
@@ -1,29 +1,30 @@
 package com.improving.app.gateway
 
-import akka.actor.{typed, Scheduler}
+import akka.actor.typed
 import akka.actor.typed.scaladsl.adapter.ClassicActorSystemOps
 import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.headers.Host
 import akka.http.scaladsl.server.Route
-import akka.util.Timeout
 
 import scala.concurrent.duration.DurationInt
-import scala.util.Random
-import com.improving.app.gateway.domain.common.Contact
-import com.improving.app.gateway.domain.MemberInfo
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
-import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.http.scaladsl.testkit.{RouteTestTimeout, ScalatestRouteTest}
 import cats.implicits.toFunctorOps
+import com.improving.app.gateway.api.handlers.MemberGatewayHandler
 import com.improving.app.gateway.domain.MemberMessages.{
+  MemberCommand,
   MemberData,
   MemberEventResponse,
   MemberRegistered,
   MemberResponse,
   RegisterMember
 }
-import com.improving.app.gateway.domain.NotificationPreference.EMAIL_NOTIFICATION_PREFERENCE
-import com.improving.app.gateway.infrastructure.GatewayServerImpl
-import io.circe.Decoder
+import com.improving.app.gateway.domain.common.util.memberInfoToGatewayMemberInfo
+import com.improving.app.gateway.infrastructure.routes.MemberGatewayRoutes
+import com.improving.app.member.domain.TestData.baseMemberInfo
+import com.typesafe.config.{Config, ConfigFactory}
+import io.circe.{Decoder, Encoder}
 import io.circe.syntax._
 import io.circe.generic.auto._
 import org.scalatest.BeforeAndAfterEach
@@ -37,7 +38,12 @@ class GatewayServerSpec
     with Matchers
     with ScalatestRouteTest
     with ScalaFutures
-    with BeforeAndAfterEach {
+    with BeforeAndAfterEach
+    with MemberGatewayRoutes {
+  implicit val encodeMemberCommand: Encoder[MemberCommand] = Encoder.instance {
+    case response @ RegisterMember(_, _, _) =>
+      response.asJson
+  }
 
   implicit val decodeMemberResponse: Decoder[MemberResponse] =
     List[Decoder[MemberResponse]](
@@ -50,47 +56,34 @@ class GatewayServerSpec
       Decoder[MemberData].widen
     ).reduceLeft(_ or _)
 
-  implicit val timeout: Timeout = Timeout(500.milliseconds)
+  implicit val timeout: RouteTestTimeout = RouteTestTimeout(10.seconds)
 
   implicit val typedSystem: typed.ActorSystem[Nothing] = system.toTyped
-  private val server: GatewayServerImpl = new GatewayServerImpl()
 
-  implicit val scheduler: Scheduler = system.scheduler
+  override val config: Config = ConfigFactory.load("application.conf")
 
-  override def beforeEach(): Unit = server.start()
-  override def afterEach(): Unit = server.sys.terminate()
+  implicit override val handler: MemberGatewayHandler = new MemberGatewayHandler()
 
+  implicit val defaultHostInfo: DefaultHostInfo = DefaultHostInfo(Host.apply("localhost"), securedConnection = false)
   "In MemberGateway" when {
     "Sending RegisterMember" should {
       "succeed" in {
-        val info = MemberInfo(
-          handle = Random.nextString(31),
-          avatarUrl = Random.nextString(31),
-          firstName = Random.nextString(31),
-          lastName = Random.nextString(31),
-          notificationPreference = EMAIL_NOTIFICATION_PREFERENCE,
-          notificationOptIn = true,
-          contact = Contact(
-            firstName = Random.nextString(31),
-            lastName = Random.nextString(31),
-            emailAddress = Some(Random.nextString(31)),
-            phone = Some(Random.nextString(31)),
-            userName = Random.nextString(31)
-          ),
-          organizations = Seq(UUID.randomUUID()),
-          tenant = UUID.randomUUID()
-        )
+        val info = memberInfoToGatewayMemberInfo(baseMemberInfo)
 
+        val memberId = UUID.randomUUID()
         val registeringMember = UUID.randomUUID()
 
-        val command = RegisterMember(info, registeringMember)
+        val command = RegisterMember(memberId, info, registeringMember)
 
-        Post("/register", command.asJson.toString()) -> Route.seal(server.routes) -> check {
+        Post("/member", command.asInstanceOf[MemberCommand].asJson) ~> Route.seal(
+          routes
+        ) ~> check {
           status shouldEqual StatusCodes.Success
           responseEntity.toString.asJson.as[MemberEventResponse].map { response =>
             val registered = response.asInstanceOf[MemberRegistered]
+            registered.memberId shouldEqual memberId
             registered.memberInfo shouldEqual info
-            registered.actingMember shouldEqual registeringMember
+            registered.metaInfo.createdBy shouldEqual registeringMember
           }
         }
       }
