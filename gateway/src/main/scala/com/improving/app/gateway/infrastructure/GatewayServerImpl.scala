389a0cc5b879fe8d79b3b59ad37bc71402a4ec2f
alexweinstein <alex.weinstein@improving.com>
2023-04-14 14:46:47 -0400

    fix: testcontainers working (?) [IA-161]

--- gateway/src/main/scala/com/improving/app/gateway/infrastructure/GatewayServerImpl.scala
+++ gateway/src/main/scala/com/improving/app/gateway/infrastructure/GatewayServerImpl.scala
@@ -7,8 +7,7 @@ import com.improving.app.gateway.domain.common.util.getHostAndPortForService
 import com.improving.app.gateway.infrastructure.routes.MemberGatewayRoutes
 import com.typesafe.config.{Config, ConfigFactory}
 
-import scala.concurrent.duration.DurationInt
-import scala.concurrent.{Await, ExecutionContextExecutor, Future}
+import scala.concurrent.ExecutionContextExecutor
 import scala.util.{Failure, Success}
 
 class GatewayServerImpl(implicit val sys: ActorSystem[_]) extends MemberGatewayRoutes {
@@ -17,15 +16,14 @@ class GatewayServerImpl(implicit val sys: ActorSystem[_]) extends MemberGatewayR
     .load("application.conf")
     .withFallback(ConfigFactory.defaultApplication())
 
-  val handler: MemberGatewayHandler = new MemberGatewayHandler()
+  implicit override val handler: MemberGatewayHandler = new MemberGatewayHandler(
+    getHostAndPortForService("member-service")
+  )
 
   implicit val dispatcher: ExecutionContextExecutor = sys.dispatchers.lookup(DispatcherSelector.defaultDispatcher())
-
-  private val binding = Http()
+  def start(): Unit = Http()
     .newServerAt(config.getString("akka.http.interface"), config.getInt("akka.http.port"))
-    .bindFlow(routes(handler))
-
-  def start(): Unit = binding
+    .bindFlow(routes)
     .onComplete {
       case Success(binding) =>
         val address = binding.localAddress
@@ -33,12 +31,5 @@ class GatewayServerImpl(implicit val sys: ActorSystem[_]) extends MemberGatewayR
       case Failure(ex) =>
         println(s"Failed to bind HTTP endpoint for Improving.APP Gateway, terminating system: ${ex.getMessage}")
         sys.terminate()
-    }
-
-  def tearDown: Future[Unit] = Await
-    .result(binding, 10.seconds)
-    .terminate(hardDeadline = 5.seconds)
-    .flatMap { _ =>
-      Future(sys.terminate())
     }
 }
