f34278a878cf5ee31d5ba5da4b8831764601c695
Dag Arneson <127801986+dag-yoppworks@users.noreply.github.com>
2023-05-19 14:59:13 -0500

    Handle optional updates idiomatically (#39)

--- tenant/src/main/scala/com/improving/app/tenant/domain/util.scala
+++ tenant/src/main/scala/com/improving/app/tenant/domain/util.scala
@@ -1,26 +1,18 @@
 package com.improving.app.tenant.domain
 
-import com.improving.app.common.domain
 import com.improving.app.common.domain.util.{addressFromEditableAddress, contactFromEditableContact}
-import com.improving.app.common.domain.{Address, Contact, EditableAddress, EditableContact}
-import com.improving.app.common.service.util.{doForSameIfHas, doIfHas}
 
 object util {
   def infoFromEditableInfo(infoToUpdate: EditableTenantInfo, infoToCopy: TenantInfo): TenantInfo =
     infoToCopy
       .copy(
-        name = doForSameIfHas[String](infoToUpdate.name, infoToCopy.name),
-        address = doIfHas[EditableAddress, Address](
-          infoToUpdate.address,
-          infoToCopy.address,
-          v => addressFromEditableAddress(v, infoToCopy.address)
-        ),
-        primaryContact = doIfHas[EditableContact, Contact](
-          infoToUpdate.primaryContact,
-          infoToCopy.primaryContact,
-          v => contactFromEditableContact(v, infoToCopy.primaryContact)
-        ),
-        organizations =
-          if (infoToUpdate.organizations.isDefined) infoToUpdate.getOrganizations else infoToCopy.organizations
+        name = infoToUpdate.name.getOrElse(infoToCopy.name),
+        address = infoToUpdate.address
+          .map(addressFromEditableAddress(_, infoToCopy.address))
+          .getOrElse(infoToCopy.address),
+        primaryContact = infoToUpdate.primaryContact
+          .map(contactFromEditableContact(_, infoToCopy.primaryContact))
+          .getOrElse(infoToCopy.primaryContact),
+        organizations = infoToUpdate.organizations.getOrElse(infoToCopy.organizations)
       )
 }
