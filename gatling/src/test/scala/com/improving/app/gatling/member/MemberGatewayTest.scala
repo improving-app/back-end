d1593391a6d46b06f1f549dc06de39a0eab6b7e3
alexweinstein <alex.weinstein@improving.com>
2023-06-16 12:13:32 -0400

    feat: update from main - EditableMEmberInfo in RegisterMember [IA-188]

--- gatling/src/test/scala/com/improving/app/gatling/member/MemberGatewayTest.scala
+++ gatling/src/test/scala/com/improving/app/gatling/member/MemberGatewayTest.scala
@@ -2,7 +2,7 @@ package com.improving.app.gatling.member
 
 import akka.http.scaladsl.model.ContentTypes
 import com.improving.app.common.domain.{Contact, MemberId, OrganizationId, TenantId}
-import com.improving.app.gateway.domain.{MemberInfo, NotificationPreference, RegisterMember}
+import com.improving.app.gateway.domain.{EditableMemberInfo, NotificationPreference, RegisterMember}
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
@@ -21,29 +21,34 @@ class MemberGatewayTest extends Simulation {
         .headers(Map("Content-Type" -> ContentTypes.`application/json`.toString()))
         .body(
           StringBody(
-            s"""\"${JsonFormat.toJsonString(
-              RegisterMember(
-                MemberId(UUID.randomUUID().toString),
-                MemberInfo(
-                  handle = "handle",
-                  avatarUrl = "avatarUrl",
-                  firstName = "firstName",
-                  lastName = "lastName",
-                  notificationPreference = Some(NotificationPreference.NOTIFICATION_PREFERENCE_EMAIL),
-                  notificationOptIn = true,
-                  contact = Contact(
-                    firstName = "firstName",
-                    lastName = "lastName",
-                    emailAddress = Some("email@email.com"),
-                    phone = Some("111-111-1111"),
-                    userName = "userName"
+            s"""\"${JsonFormat
+              .toJsonString(
+                RegisterMember(
+                  Some(MemberId(UUID.randomUUID().toString)),
+                  Some(
+                    EditableMemberInfo(
+                      handle = Some("handle"),
+                      avatarUrl = Some("avatarUrl"),
+                      firstName = Some("firstName"),
+                      lastName = Some("lastName"),
+                      notificationPreference = Some(NotificationPreference.NOTIFICATION_PREFERENCE_EMAIL),
+                      contact = Some(
+                        Contact(
+                          firstName = "firstName",
+                          lastName = "lastName",
+                          emailAddress = Some("email@email.com"),
+                          phone = Some("111-111-1111"),
+                          userName = "userName"
+                        )
+                      ),
+                      organizationMembership = Seq(OrganizationId(UUID.randomUUID().toString)),
+                      tenant = Some(TenantId(UUID.randomUUID().toString))
+                    )
                   ),
-                  organizationMembership = Seq(OrganizationId(UUID.randomUUID().toString)),
-                  tenant = TenantId(UUID.randomUUID().toString)
-                ),
-                MemberId(UUID.randomUUID().toString)
+                  Some(MemberId(UUID.randomUUID().toString))
+                )
               )
-            ).replace("\"", "\\\"")}\""""
+              .replace("\"", "\\\"")}\""""
           )
         )
     )
