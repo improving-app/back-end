d1631544db6c0a8c11413775aa0f3f543505279d
alexweinstein <alex.weinstein@improving.com>
2023-06-28 14:37:17 -0400

    feat: create tenants using startScenario [IA-197]

--- gateway/src/main/scala/com/improving/app/gateway/domain/common/tenantUtil.scala
+++ gateway/src/main/scala/com/improving/app/gateway/domain/common/tenantUtil.scala
@@ -1,7 +1,11 @@
 package com.improving.app.gateway.domain.common
 
+import com.improving.app.common.domain.util.{EditableAddressUtil, EditableContactUtil}
+import com.improving.app.gateway.domain.demoScenario.Tenant
 import com.improving.app.gateway.domain.tenant.{
   EditableTenantInfo => GatewayEditableTenantInfo,
+  TenantEstablished,
+  TenantInfo,
   TenantMetaInfo => GatewayTenantMetaInfo,
   TenantOrganizationList => GatewayTenantOrganizationList,
   TenantStates => GatewayTenantStates
@@ -9,6 +13,24 @@ import com.improving.app.gateway.domain.tenant.{
 import com.improving.app.tenant.domain.{EditableTenantInfo, TenantMetaInfo, TenantOrganizationList}
 
 object tenantUtil {
+
+  implicit class EstablishedTenantUtil(established: TenantEstablished) {
+    implicit def toTenant: Tenant = Tenant(
+      tenantId = established.tenantId,
+      tenantInfo = established.tenantInfo.map(_.toInfo),
+      metaInfo = established.metaInfo
+    )
+  }
+
+  implicit class GatewayEditableTenantInfoUtil(info: GatewayEditableTenantInfo) {
+    implicit def toInfo: TenantInfo = TenantInfo(
+      name = info.getName,
+      primaryContact = info.primaryContact.map(_.toContact),
+      address = info.address.map(_.toAddress),
+      organizations = info.organizations
+    )
+  }
+
   def gatewayEditableTenantInfoToEditableInfo(info: GatewayEditableTenantInfo): EditableTenantInfo = EditableTenantInfo(
     info.name,
     info.primaryContact,
--- gateway/src/main/scala/com/improving/app/gateway/infrastructure/routes/DemoScenarioGatewayRoutes.scala
+++ gateway/src/main/scala/com/improving/app/gateway/infrastructure/routes/DemoScenarioGatewayRoutes.scala
@@ -1,17 +1,26 @@
 package com.improving.app.gateway.infrastructure.routes
 
-import akka.grpc.GrpcServiceException
-import akka.http.scaladsl.model.StatusCodes.BadRequest
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpResponse}
-import akka.http.scaladsl.server.Directives.{as, complete, entity, logRequestResult, pathPrefix, post}
-import akka.http.scaladsl.server.directives.FutureDirectives.onSuccess
-import akka.http.scaladsl.server.{ExceptionHandler, Route}
+import akka.http.scaladsl.server.Directives.{as, complete, entity, logRequestResult, onSuccess, pathPrefix, post}
+import akka.http.scaladsl.server.Route
+import com.improving.app.common.domain.{EditableAddress, EditableContact, MemberId, OrganizationId, TenantId}
 import com.improving.app.gateway.api.handlers.TenantGatewayHandler
-import com.improving.app.gateway.domain.tenant.{EstablishTenant => GatewayEstablishTenant}
+import com.improving.app.gateway.domain.common.tenantUtil.EstablishedTenantUtil
+import com.improving.app.gateway.domain.demoScenario.{ScenarioStarted, StartScenario, Tenant}
+import com.improving.app.gateway.domain.tenant.{
+  EditableTenantInfo,
+  EstablishTenant,
+  TenantEstablished,
+  TenantOrganizationList
+}
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.StrictLogging
 import de.heikoseeberger.akkahttpcirce.ErrorAccumulatingCirceSupport
-import io.circe.Json
+
+import java.time.LocalDateTime
+import java.util.UUID
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
 trait DemoScenarioGatewayRoutes extends ErrorAccumulatingCirceSupport with StrictLogging {
 
@@ -21,14 +30,45 @@ trait DemoScenarioGatewayRoutes extends ErrorAccumulatingCirceSupport with Stric
     pathPrefix("demo") {
       pathPrefix("startScenario") {
         post {
-          entity(as[String]) { establishTenant =>
-            onSuccess(
-              handler
-                .establishTenant(
-                  GatewayEstablishTenant.fromAscii(establishTenant)
+          entity(as[String]) { command =>
+            val parsed = StartScenario.fromAscii(command)
+
+            val creatingMemberId = Some(MemberId(UUID.randomUUID().toString))
+            val orgId = OrganizationId(UUID.randomUUID().toString)
+
+            val establishTenantRequests = (1 to parsed.numTenants).map(_ =>
+              EstablishTenant(
+                Some(TenantId(UUID.randomUUID().toString)),
+                creatingMemberId,
+                Some(
+                  EditableTenantInfo(
+                    Some("Demo-" + LocalDateTime.now().toString),
+                    Some(
+                      EditableContact(
+                      )
+                    ),
+                    Some(EditableAddress()),
+                    Some(TenantOrganizationList(Seq(orgId)))
+                  )
                 )
-            ) { tenantEstablished =>
-              complete(tenantEstablished.toProtoString)
+              )
+            )
+
+            val requestsFut = Future
+              .sequence(
+                establishTenantRequests
+                  .map(req =>
+                    handler
+                      .establishTenant(
+                        req
+                      )
+                  )
+              )
+
+            complete {
+              requestsFut.map { reqs =>
+                ScenarioStarted(tenants = reqs.map(_.toTenant)).toProtoString
+              }
             }
           }
         }
