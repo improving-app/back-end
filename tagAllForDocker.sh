d83113c7adb0fae2be958432ac1ebc797d52cfa4
alexweinstein <alex.weinstein@improving.com>
2023-07-19 11:41:34 -0400

    feat: small things [IA-198]

--- tagAllForDocker.sh
+++ tagAllForDocker.sh
@@ -1,4 +1,4 @@
-for dockerfile in "improving-app-gateway" "improving-app-tenant" "improving-app-organization" "improving-app-member" "improving-app-store" "improving-app-event"; do
+for dockerfile in "improving-app-gateway" "improving-app-tenant" "improving-app-organization" "improving-app-member" "improving-app-store" "improving-app-event" "improving-app-product"; do
   docker tag $dockerfile\:latest weinyopp/$dockerfile\:latest
   docker push weinyopp/$dockerfile\:latest
 done
