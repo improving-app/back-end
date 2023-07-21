7ec95f06204a883f64903028c5bc128e1e7005e1
AlexWeinstein92 <105220856+AlexWeinstein92@users.noreply.github.com>
2023-07-21 10:54:59 -0400

    feat: Event Gateway handler [IA-227] (#62)

---
+++ gateway/src/main/scala/com/improving/app/gateway/api/handlers/EventGatewayHandler.scala
@@ -0,0 +1,89 @@
+package com.improving.app.gateway.api.handlers
+
+import akka.actor.typed.ActorSystem
+import akka.grpc.GrpcClientSettings
+import akka.util.Timeout
+import com.improving.app.gateway.domain.common.util.getHostAndPortForService
+import com.improving.app.gateway.domain.event.{
+  CancelEvent => GatewayCancelEvent,
+  CreateEvent => GatewayCreateEvent,
+  EventCancelled,
+  EventCreated,
+  EventScheduled,
+  ScheduleEvent => GatewayScheduleEvent
+}
+import com.improving.app.gateway.domain.eventUtil._
+import com.improving.app.event.api.EventServiceClient
+import com.improving.app.event.domain.{CancelEvent, CreateEvent, ScheduleEvent}
+import com.typesafe.scalalogging.StrictLogging
+
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{ExecutionContext, Future}
+import scala.language.postfixOps
+
+class EventGatewayHandler(grpcClientSettingsOpt: Option[GrpcClientSettings] = None)(implicit
+    val system: ActorSystem[_]
+) extends StrictLogging {
+  implicit val ec: ExecutionContext = system.executionContext
+  implicit val timeout: Timeout = Timeout(5 minute)
+
+  protected val (clientHost, clientPort) = getHostAndPortForService("event-service")
+
+  private val eventClient: EventServiceClient = EventServiceClient(
+    grpcClientSettingsOpt.getOrElse(
+      GrpcClientSettings
+        .connectToServiceAt(clientHost, clientPort)
+        .withTls(false)
+    )
+  )
+
+  def createEvent(in: GatewayCreateEvent): Future[EventCreated] =
+    eventClient
+      .createEvent(
+        CreateEvent(
+          in.eventId,
+          in.info.map(_.toEditableInfo),
+          in.onBehalfOf
+        )
+      )
+      .map { response =>
+        EventCreated(
+          response.eventId,
+          response.info.map(_.toGatewayEditableInfo),
+          response.meta.map(_.toGatewayEventMeta)
+        )
+      }
+
+  def scheduleEvent(in: GatewayScheduleEvent): Future[EventScheduled] =
+    eventClient
+      .scheduleEvent(
+        ScheduleEvent(
+          in.eventId,
+          None,
+          in.onBehalfOf
+        )
+      )
+      .map { response =>
+        EventScheduled(
+          response.eventId,
+          response.info.map(_.toGatewayEditableInfo.toInfo),
+          response.meta.map(_.toGatewayEventMeta)
+        )
+      }
+
+  def cancelEvent(in: GatewayCancelEvent): Future[EventCancelled] =
+    eventClient
+      .cancelEvent(
+        CancelEvent(
+          in.eventId,
+          "Cancellation reason",
+          in.onBehalfOf
+        )
+      )
+      .map { response =>
+        EventCancelled(
+          response.eventId,
+          response.meta.map(_.toGatewayEventMeta)
+        )
+      }
+}
