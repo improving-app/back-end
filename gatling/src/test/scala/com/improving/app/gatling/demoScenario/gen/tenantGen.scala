a4d0e226d841b5ceb2ccb85b0b73083596d75ea2
alexweinstein <alex.weinstein@improving.com>
2023-07-28 09:50:15 -0400

    feat: gatling util -> gen [IA-236]

--- gatling/src/test/scala/com/improving/app/gatling/demoScenario/gen/tenantGen.scala
+++ gatling/src/test/scala/com/improving/app/gatling/demoScenario/gen/tenantGen.scala
@@ -1,59 +1,39 @@
-package com.improving.app.gatling.demoScenario
+package com.improving.app.gatling.demoScenario.gen
 
-import com.improving.app.common.domain.{
-  EditableAddress,
-  EditableContact,
-  MemberId,
-  OrganizationId,
-  PostalCodeMessageImpl,
-  TenantId,
-  UsPostalCodeImpl
-}
+import com.improving.app.common.domain._
 import com.improving.app.gateway.domain.tenant.{
   ActivateTenant,
   EditableTenantInfo,
   EstablishTenant,
   TenantOrganizationList
 }
-import com.improving.app.gatling.common.util.{genPhoneNumber, genPostalCode}
+import com.improving.app.gatling.common.gen._
 
 import java.time.LocalDateTime
 import java.util.UUID
-import scala.io.Source
 import scala.util.Random
 
-object util {
-
-  private val firstNamesFile = "firstNames.txt"
-  private val lastNamesFile = "lastNames.txt"
-  private val cityStatesFile = "fakariaCityStates.txt"
-  private val addressesFile = "addresses.txt"
-
-  private val firstNames: Seq[String] = Source.fromResource(firstNamesFile).getLines().toSeq
-
-  private val lastNames: Seq[String] = Source.fromResource(lastNamesFile).getLines().toSeq
-
-  private val addresses: Seq[String] = Source.fromResource(addressesFile).getLines().toSeq
-
-  private val cityStates: Seq[Seq[String]] =
-    Source.fromResource(cityStatesFile).getLines().toSeq.map { _.split(",").toSeq }
-
+object tenantGen {
   def genEstablishTenantReqs(
-      creatingMembersForTenant: Seq[(Some[MemberId], Some[TenantId])],
+      creatingMembers: Seq[Some[MemberId]],
       numTenants: Int,
       orgId: OrganizationId
-  ): Seq[EstablishTenant] = firstNames
+  ): Seq[EstablishTenant] = Random
+    .shuffle(firstNames)
     .zip(Random.shuffle(lastNames))
     .zip(Random.shuffle(addresses))
     .zip(Random.shuffle(cityStates))
     .take(numTenants)
-    .zip(creatingMembersForTenant.flatMap(_._1))
+    .zip(creatingMembers)
     .flatMap {
-      case ((((first: String, last: String), address: String), Seq(city: String, state: String)), member: MemberId) =>
+      case (
+            (((first: String, last: String), address: String), Seq(city: String, state: String)),
+            member: Option[MemberId]
+          ) =>
         Some(
           EstablishTenant(
             Some(TenantId(UUID.randomUUID().toString)),
-            Some(member),
+            member,
             Some(
               EditableTenantInfo(
                 Some("Demo-" + LocalDateTime.now().toString),
